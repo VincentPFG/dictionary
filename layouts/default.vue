174d1afefe2bad16105e40f9baf8fdd90bfbde0b
VincentPFG <vincentgiraud@live.fr>
2022-09-22 13:57:48 +0200

    final

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,34 +1,3 @@
 <template>
-  <v-app dark>
-    <v-app-bar
-      app
-      dense
-    >
-      <v-spacer />
-      <v-form>
-        <v-text-field
-          dense
-          name="word"
-          outlined
-          autofocus
-          hide-details
-          autocapitalize="none"
-        />
-      </v-form>
-      <v-spacer />
-      <v-btn-toggle multiple>
-        <v-btn to="en">
-          EN
-        </v-btn>
-        <v-btn to="es">
-          ES
-        </v-btn>
-      </v-btn-toggle>
-    </v-app-bar>
-    <v-main>
-      <!-- <v-container> -->
-      <Nuxt />
-      <!-- </v-container> -->
-    </v-main>
-  </v-app>
+  <Nuxt />
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,34 @@
 <template>
   <v-app dark>
-    <v-carousel v-if="language == 0" height="100%">
+    <v-carousel v-if="language == 0" v-model="slide" height="100%">
       <v-carousel-item>
         <iframe :src="`https://www.merriam-webster.com/dictionary/${word}`" />
       </v-carousel-item>
       <v-carousel-item>
-        <iframe :src="`https://en.wiktionary.org/wiki/${word}#Pronunciation`" />
-      </v-carousel-item>
-      <v-carousel-item>
         <iframe :src="`https://www.wordreference.com/enfr/${word}`" />
       </v-carousel-item>
+      <v-carousel-item>
+        <iframe :src="`https://www.wordreference.com/enes/${word}`" />
+      </v-carousel-item>
+      <v-carousel-item>
+        <iframe :src="`https://en.wiktionary.org/wiki/${word}#Pronunciation`" />
+      </v-carousel-item>
     </v-carousel>
-    <v-carousel v-if="language == 1" height="100%">
+    <v-carousel v-if="language == 1" v-model="slide" height="100%">
       <v-carousel-item>
         <iframe :src="`https://www.wordreference.com/esfr/${word}`" />
+      </v-carousel-item>
+      <v-carousel-item>
+        <iframe :src="`https://www.wordreference.com/esen/${word}`" />
+      </v-carousel-item>
+      <v-carousel-item>
+        <iframe :src="`https://www.wordreference.com/conj/esverbs.aspx?v=${word}`" />
       </v-carousel-item>
       <v-carousel-item>
         <iframe :src="`https://es.wiktionary.org/wiki/${word}#Etimología`" />
       </v-carousel-item>
     </v-carousel>
-    <v-carousel v-if="language == 2" height="100%">
+    <v-carousel v-if="language == 2" v-model="slide" height="100%">
       <v-carousel-item>
         <iframe :src="`https://www.wordreference.com/fren/${word}`" />
       </v-carousel-item>
@@ -27,13 +36,13 @@
         <iframe :src="`https://www.wordreference.com/fres/${word}`" />
       </v-carousel-item>
       <v-carousel-item>
+        <iframe :src="`https://www.wordreference.com/conj/frverbs.aspx?v=${word}`" />
+      </v-carousel-item>
+      <v-carousel-item>
         <iframe :src="`https://fr.wiktionary.org/wiki/${word}`" />
       </v-carousel-item>
     </v-carousel>
-    <v-app-bar
-
-      dense
-    >
+    <v-app-bar dense>
       <v-spacer />
       <v-text-field
         dense
@@ -62,18 +71,23 @@
 <script>
 
 export default {
-  layout: 'empty',
   data () {
-    return { word: '', language: [], search: '' }
+    return { word: '', language: [], slide: 0 }
+  },
+  watch: {
+    language () { this.slide = 0 }
   }
 }
 </script>
 
 <style>
 iframe {
-    border: 0;
-    width: 100%;
-    height: 100%;
+  border: 0;
+  width: 100%;
+  height: 100%;
 }
-body{height: 100%;}
+
+body {
+  height: 100%;
+}
 </style>
